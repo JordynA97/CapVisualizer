19b0ce90da3e4239b1974fcd3f3cc24c1f3d14be
JordynA97 <jla5483@rit.edu>
2020-02-07 23:20:23 -0500

    slider research

--- script.js
+++ script.js
@@ -12,9 +12,35 @@ function checkVariables(){
     if(varOne.value >= 5){
         badImg.style.visibility = "hidden";
         goodImg.style.visibility = "visible";
+        
     }else if(varOne.value < 5){
         badImg.style.visibility = "visible";
         goodImg.style.visibility = "hidden"
     }
+
+    //values can be linked together
+    //varTwo.value = varOne.value;
+
+    //this lets one slider change the steps of 
+    // if(varOne.value > 5){
+    //    varTwo.step = 5;
+    // }
+
+    //if value is 5, other sliders max is changed.
+    // if(varOne.value == 5){
+    //    varTwo.max = 5;
+    // }
+
+    //if slider is 0, disable the other slider.
+    // if(varOne.value == 0){
+    //     varTwo.disabled = true;
+    // }
+
+    //https://bashooka.com/coding/25-amazing-css-range-slider-designs/
+
+    //can possibly lock at value and disable and tell user it cant change unless sliders moved
+
+    //moves sliders to specific spot based on other sliders value
+    //https://jsfiddle.net/fe7j3ohv/28/
 }
 
