5653bee5db0b16737e226f13216ecbd637f3dfee
JordynA97 <jla5483@rit.edu>
2020-02-10 12:34:30 -0500

    serverside

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+const fs = require('fs');
+
+const html = fs.readFileSync(`${__dirname}/../VisualizerHTML.html`);
+
+const getHtml = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(html);
+  response.end();
+};
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+
+  switch (request.url) {
+    case '/':
+      getHtml(request, response);
+      break;
+    default:
+      getHtml(request, response);
+      break;
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log('listening locally');
