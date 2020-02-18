7d9e7cc8df1e4fceac6f7b0449d06500e1d2f779
JordynA97 <jla5483@rit.edu>
2020-02-18 09:40:04 -0500

    back to square one

--- src/script.js
+++ src/script.js
@@ -17,29 +17,5 @@ function checkVariables(){
         goodImg.style.visibility = "hidden"
     }
 
-    //values can be linked together
-    //publicTransport.value = population.value;
-
-    //this lets one slider change the steps of 
-    // if(population.value > 5){
-    //    publicTransport.step = 5;
-    // }
-
-    //if value is 5, other sliders max is changed.
-    // if(population.value == 5){
-    //    publicTransport.max = 5;
-    // }
-
-    //if slider is 0, disable the other slider.
-    // if(population.value == 0){
-    //     publicTransport.disabled = true;
-    // }
-
-    //https://bashooka.com/coding/25-amazing-css-range-slider-designs/
-
-    //can possibly lock at value and disable and tell user it cant change unless sliders moved
-
-    //moves sliders to specific spot based on other sliders value
-    //https://jsfiddle.net/fe7j3ohv/28/
 }
 
