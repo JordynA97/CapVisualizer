af592409f10bca0db46726f3157448ffe21a8073
JordynA97 <jla5483@rit.edu>
2020-02-27 09:22:32 -0500

    sliders erased, new sliders in

--- src/script.js
+++ src/script.js
@@ -1,23 +1,3 @@
-//script to check the sliders and display image based on that
-
-function checkVariables(){
-    //variable sliders
-    let populationSlider = document.getElementById("population");
-
-    //outcome animations
-    let badImg = document.getElementById("badEndingImg");
-    let goodImg = document.getElementById("goodEndingImg");
-
-    if(populationSlider.value >= 5){
-        badImg.style.visibility = "hidden";
-        goodImg.style.visibility = "visible";
-        
-    }else if(populationSlider.value <= 5){
-        badImg.style.visibility = "visible";
-        goodImg.style.visibility = "hidden"
-    }
-
-}
 
 var id = 1;
 
