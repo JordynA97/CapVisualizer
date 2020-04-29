f0210e34d02820801e4a8bad50fc5df63efc5185
JordynA97 <jla5483@rit.edu>
2020-04-28 20:42:08 -0400

    stats overlay style complete

--- src/script.js
+++ src/script.js
@@ -1,14 +1,22 @@
 var sidePanel = document.getElementById("mySidepanel");
 var sidePanelRight = document.getElementById("mySidepanelRight");
 
+//inner navs
+var innerOverlayOne = document.getElementById("innerOverlay1");
+var innerOverlayTwo = document.getElementById("innerOverlay2");
+var innerOverlayThree = document.getElementById("innerOverlay3");
+var innerOverlayFour = document.getElementById("innerOverlay4");
+
 //button function for left panel
 function openNav() {
-  if(sidePanel.style.width == "35%"){
+  if(sidePanel.style.width == "100%"){
     closeNav();
   }else{
-  sidePanel.style.width = "35%";
-  sidePanel.style.height = "100%";
+  sidePanel.style.width = "100%";
+  sidePanel.style.height = "30vh";
   }
+
+  //statsButton.style.bottom = "100%";
 }
 
 function closeNav() {
@@ -31,6 +39,68 @@ function closeNavRight() {
   sidePanelRight.style.width = "0";
   sidePanelRight.style.height = "0";
 }
+
+//inner nav one
+function openInnerOne(){
+  if(innerOverlayOne.style.width == "20vw"){
+    closeInnerNavOne();
+  }else{
+    innerOverlayOne.style.width="20vw";
+    innerOverlayOne.style.height = "18vh";
+  }
+}
+
+function closeInnerNavOne(){
+  innerOverlayOne.style.height = "0";
+  innerOverlayOne.style.width = "20%";
+}
+
+//inner nav two
+function openInnerTwo(){
+  if(innerOverlayTwo.style.width == "20vw"){
+    closeInnerNavTwo();
+  }else{
+    innerOverlayTwo.style.width="20vw";
+    innerOverlayTwo.style.height = "18vh";
+  }
+}
+
+function closeInnerNavTwo(){
+  innerOverlayTwo.style.height = "0";
+  innerOverlayTwo.style.width = "20%";
+}
+
+//inner nav 3
+function openInnerThree(){
+  if(innerOverlayThree.style.width == "20vw"){
+    closeInnerNavThree();
+  }else{
+    innerOverlayThree.style.width="20vw";
+    innerOverlayThree.style.height = "18vh";
+  }
+}
+
+function closeInnerNavThree(){
+  innerOverlayThree.style.height = "0";
+  innerOverlayThree.style.width = "20%";
+}
+
+//inner nav 4
+function openInnerFour(){
+  if(innerOverlayFour.style.width == "20vw"){
+    closeInnerNavFour();
+  }else{
+    innerOverlayFour.style.width="20vw";
+    innerOverlayFour.style.height = "18vh";
+  }
+}
+
+function closeInnerNavFour(){
+  innerOverlayFour.style.height = "0";
+  innerOverlayFour.style.width = "20%";
+}
+
+
 
 
 function corpsChange(e) {
